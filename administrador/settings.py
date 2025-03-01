5fa5ebef885d8a4d2bff625e997d6139c6cfdc0e
ivantolavillca <tolavillcaivan7@gmail.com>
2025-03-01 18:47:19 -0400

    segundo commit

--- administrador/settings.py
+++ administrador/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,14 +100,7 @@ WSGI_APPLICATION = 'administrador.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'db_prueba_oahu',           
-        'USER': 'db_prueba_oahu_user',                  
-        'PASSWORD': 'IGvQoQfa9AT2EjqdKMmWnqsUI3WzOIuF',          
-        'HOST': 'dpg-cv1of852ng1s738jcfag-a',                  
-        'PORT': '5432', 
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
