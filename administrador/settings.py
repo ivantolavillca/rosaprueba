b84ebf26d81e36c8820f4861a0ba71e297f3dee9
ivantolavillca <tolavillcaivan7@gmail.com>
2025-03-01 19:55:58 -0400

    cambios en ccs

--- administrador/settings.py
+++ administrador/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4t@_@yr$rrb@&#x_h7l@zo83afo3+kb6jk_%gxsun0y4)e*mc!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -71,9 +71,10 @@ SWAGGER_SETTINGS = {
     "USE_SESSION_AUTH": False,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "*",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'administrador.urls'
 
